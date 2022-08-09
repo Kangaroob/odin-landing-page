89e4ffffc168cf1678dac7ff99b6a279e310576d
QSL <--global>
2022-08-08 21:17:28 -0700

    Adjust viewsetter trigger

--- viewSetter.js
+++ viewSetter.js
@@ -2,7 +2,7 @@ const styleSet = document.querySelector('#style');
 const titleSet = document.getElementById('title');
 const exampleImageSet = document.getElementById('exampleImage');
 
-if (window.outerWidth < window.outerHeight) {
+if (window.outerWidth < 840) {
     styleSet.setAttribute("href", "./styleMobile.css");
     titleSet.insertBefore(exampleImageSet, titleSet.children[1]);
     titleSet.classList.add("flex");
