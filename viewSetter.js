19c3d3774fcb738ca7e381c80581b3da6f818594
QSL <--global>
2022-08-08 21:54:00 -0700

    Add viewStyleToggle

--- viewSetter.js
+++ viewSetter.js
@@ -1,10 +1,27 @@
-const styleSet = document.querySelector('#style');
-const titleSet = document.getElementById('title');
-const exampleImageSet = document.getElementById('exampleImage');
+const viewStyleToggleSet = document.getElementById("viewStyleToggle");
+viewStyleToggleSet.addEventListener("click", swapViewStyle);
+
+function swapViewStyle() {
+    const styleSet = document.querySelector('#style');
+    const titleSet = document.getElementById('title');
+    const topSet = document.getElementById('top');
+    const exampleImageSet = document.getElementById('exampleImage');
+    const currentStyleSet = styleSet.getAttribute("href");
+    if (currentStyleSet === "style.css") {
+        styleSet.setAttribute("href", "styleMobile.css");
+        titleSet.insertBefore(exampleImageSet, titleSet.children[1]);
+        titleSet.classList.add("flex");
+        titleSet.classList.add("column");
+        viewStyleToggleSet.innerHTML = "Desktop View"
+    } else if (currentStyleSet === "styleMobile.css") {
+        styleSet.setAttribute("href", "style.css");
+        topSet.insertBefore(exampleImageSet, null);
+        titleSet.classList.remove("flex");
+        titleSet.classList.remove("column");
+        viewStyleToggleSet.innerHTML = "Mobile View"
+    }
+}
 
 if (window.outerWidth < 840) {
-    styleSet.setAttribute("href", "./styleMobile.css");
-    titleSet.insertBefore(exampleImageSet, titleSet.children[1]);
-    titleSet.classList.add("flex");
-    titleSet.classList.add("column");
+    swapViewStyle();
 }
